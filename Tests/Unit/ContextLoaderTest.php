29ff10a2b3ac1ae74f4780da64ed3601cedaaaf1
Francois Suter <francois@typo3.org>
2015-06-05 11:49:15 +0200

    [TASK] Update unit tests
    Moved to namespaces and proper file stucture.
    
    Resolves: #67308
    Releases: 2.0
    Change-Id: I65240995893ed86aa2151550e5235c4658b625f9
    Reviewed-on: http://review.typo3.org/39969
    Reviewed-by: Francois Suter <francois@typo3.org>
    Tested-by: Francois Suter <francois@typo3.org>

---
+++ Tests/Unit/ContextLoaderTest.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Cobweb\Context\Tests\Unit;
+
+/**
+ * This file is part of the TYPO3 CMS project.
+ *
+ * It is free software; you can redistribute it and/or modify it under
+ * the terms of the GNU General Public License, either version 2
+ * of the License, or any later version.
+ *
+ * For the full copyright and license information, please read the
+ * LICENSE.txt file that was distributed with this source code.
+ *
+ * The TYPO3 project - inspiring people to share!
+ */
+
+use TYPO3\CMS\Core\Tests\UnitTestCase;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
+
+/**
+ * Testcase for the context loader
+ *
+ * @author Francois Suter <typo3@cobweb.ch>
+ * @package TYPO3
+ * @subpackage tx_context
+ */
+class ContextLoaderTest extends UnitTestCase {
+
+	/**
+	 * Provides values to parse and the expected result.
+	 *
+	 * @return array
+	 */
+	public function valuesProvider() {
+		$values = array(
+			// Value should be unchanged
+			'simple value' => array(
+				'value' => 'foo',
+				'result' => 'foo'
+			),
+			// Value should be stripped of the "foo:" prefix
+			'value with table' => array(
+				'value' => 'foo:bar',
+				'result' => 'bar'
+			),
+			// An array of value should remain an array, with prefixed values cleaned up
+			'array of values' => array(
+				'value' => array(
+					'foo' => 'bar',
+					'baz' => array(
+						'say' => 'hello:world'
+					)
+				),
+				'result' => array(
+					'foo' => 'bar',
+					'baz' => array(
+						'say' => 'world'
+					)
+				)
+			)
+		);
+		return $values;
+	}
+
+	/**
+	 * Test a simple value. It should be unchanged.
+	 *
+	 * @param mixed $value Value to test
+	 * @param mixed $result Expected result
+	 * @test
+	 * @dataProvider valuesProvider
+	 */
+	public function simpleValue($value, $result) {
+		/** @var \Cobweb\Context\ContextLoader $parser */
+		$parser = GeneralUtility::makeInstance('Cobweb\\Context\\ContextLoader');
+		$actualResult = $parser->cleanUpValues($value);
+		$this->assertEquals($result, $actualResult);
+	}
+}
